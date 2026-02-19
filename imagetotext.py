bdca6e9764a4101cc302ae6c648c758e032fdd7d
Codeguruu <namananilgoyal@gmail.com>
2026-02-19 16:49:00 +0530

    Updating the repo

--- imagetotext.py
+++
@@ -1,8 +0,0 @@
-import pytesseract as tess
-from PIL import Image
-
-tess.pytesseract.tesseract_cmd = r'C:\\Users\\naman\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
-
-img = Image.open('image.png')
-text = tess.image_to_string(img)
-print(text)
